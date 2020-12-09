89002c6944400118df1043057196c5fd7a343aa4
lucaslidorio <lucaslidorio@hotmail.com>
2020-12-08 21:48:29 -0400

    aula 07 gestão de planos

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::delete('admin/plans/{url}', 'Admin\PlanController@destroy')->name('plans.destroy');
+Route::get('admin/plans/create', 'Admin\PlanController@create')->name('plans.create');
+Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show');
+Route::post('admin/plans','Admin\PlanController@store')->name('plans.store');
+
+Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
 
 Route::get('/', function () {
     return view('welcome');
